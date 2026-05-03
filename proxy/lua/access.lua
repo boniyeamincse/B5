8d6a94c846eec0ca9a9c17d1c907e46d4a757e20
boniakij <boni@akijgroup.co>
2026-05-03 15:50:17 +0600

    feat(proxy): add Redis pooling and request metadata module

--- proxy/lua/access.lua
+++ proxy/lua/access.lua
@@ -2,13 +2,10 @@
 -- This script runs for every incoming request
 
 local b5_config = _G.B5_CONFIG
--- Task 3: Extract Request Metadata
-local metadata = {
 local request_metadata = require("request_metadata")
 local metadata = request_metadata.extract()
--- Helper function to log security events
+
 local function log_event(attack_type, detail)
-    ngx.log(ngx.WARN, "[B5 WAF Block] Type: ", attack_type, ", IP: ", metadata.ip, ", URI: ", metadata.uri, ", Detail: ", detail)
     ngx.log(
         ngx.WARN,
         "[B5 WAF Block] Type: ",
@@ -22,9 +19,8 @@ local function log_event(attack_type, detail)
         ", Detail: ",
         detail
     )
+end
 
--- 1. Check URI for SQLi and XSS
-if metadata.uri then
 if metadata.uri ~= "" then
     -- Check SQLi
     for _, pattern in ipairs(b5_config.sql_patterns) do
